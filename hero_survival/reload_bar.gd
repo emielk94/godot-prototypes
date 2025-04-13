9eb9a2a2d2f3853909d3c845545da18388d4a9a2
emielk94 <emielk94@gmail.com>
2025-04-13 17:01:43 -0300

    Add reloadbar scene

---
+++ hero_survival/reload_bar.gd
@@ -0,0 +1,15 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide() # Replace with function body.
+
+func set_value(value):
+	$TextureProgressBar.value = value
+	if value > 0:
+		show()
+	else:
+		hide()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
