9fe02e44a3f62844cbc2f5ce35cdcfd2a4ca6a9b
emielk94 <emielk94@gmail.com>
2025-04-16 12:05:06 -0300

    Make spawner relative to player

--- hero_survival/spawner.gd
+++ hero_survival/spawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var enemy_scenes = {"ogre": preload("res://scenes/enemies/ogre.tscn")}
+@onready var player = owner.find_child("player")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,8 +17,14 @@ func spawn(x,y, isRandomPos, count, enemy_type):
 		
 		var ogre_instance = enemy_scenes[enemy_type].instantiate()
 		if isRandomPos:
-			ogre_instance.position = Vector2(xpos, ypos) 
+			var direction = get_random_direction_2d() 
+			print(direction)
+			ogre_instance.position = player.position + direction * 300
 		else:
 			ogre_instance.position = Vector2(x, y) 
 	
 		get_tree().current_scene.add_child(ogre_instance)
+
+func get_random_direction_2d() -> Vector2:
+	var angle = randf() * TAU  # TAU is 2 * PI
+	return Vector2(cos(angle), sin(angle))
